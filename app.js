57c223acdb0e2649a6edb286a4ba7adf744aa0ae
Arvind Kumar <arvindsep18@gmail.com>
2022-10-01 15:27:24 +0530

    FRONTEND progress

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express =  require("express");
 const path = require("path");
 const app = express();
+<<<<<<< HEAD
+=======
+
+app.use(express.static('public'));
+
+>>>>>>> a54d33b... frontend progress
 app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname + "/index.html"));
 })
 
+<<<<<<< HEAD
 const server = app.listen(5000);
 const portNumber = server.address().port;
 console.log(`port is open on ${portNumber}`);
 
+=======
+const server = app.listen(5000);
+>>>>>>> a54d33b... frontend progress
